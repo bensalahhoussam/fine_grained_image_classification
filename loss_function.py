00ef4f5e615ff192878338f48ff79cb423a5a1ec
houssem_eddine_bensalah <112108580+bensalahhoussam@users.noreply.github.com>
2022-10-08 08:09:04 +0100

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -7,7 +7,29 @@ import keras.backend as K
 
 
 
+def categorical_focal_loss(gamma=2.0, alpha=0.25):
+    def focal_loss(y_true, y_pred):
+        epsilon = K.epsilon()
 
+        y_pred = K.clip(y_pred, epsilon, 1.0 - epsilon)
+        # Calculate cross entropy
+        cross_entropy = -y_true * K.log(y_pred)
+        # Calculate weight that consists of  modulating factor and weighting factor
+        weight = alpha * y_true * K.pow((1 - y_pred), gamma)
+        # Calculate focal loss
+        loss = weight * cross_entropy
+        # Sum the losses in mini_batch
+        loss = K.sum(loss, axis=1)
+        return loss
+
+    return focal_loss
+
+def categorical_label_smoothing(factor):
+    def label_smoothing(y_actual,y_output):
+        y_true_smooth = (1 - factor) * y_actual + (factor / 5.)
+        categorical_crossentropy = -1 * y_true_smooth * K.log(y_output)
+        return categorical_crossentropy 
+    return label_smoothing
 
 
 def categorical_focal_loss_with_label_smoothing(alpha, factor, gamma):
